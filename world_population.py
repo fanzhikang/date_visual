63a4ef43077e7d07e917f6eff2c8a968964e8bf7
Fzk <852672780@qq.com>
2017-12-16 20:10:05 +0800

    第二次push

--- world_population.py
+++ world_population.py
@@ -1,5 +1,6 @@
 import json 
 filename = 'population_data.json'
+from country_codes import get_country_code
 with open(filename) as f:
 	pop_data = json.load(f)
 
@@ -7,4 +8,7 @@ for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
 		country_name = pop_dict['Country Name']
 		population =int(float(pop_dict['Value']))
+		code = get_country_code(country_name)
+		if code:
+			
 		print(country_name+':'+ str(population))
